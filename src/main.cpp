c505b9fbc5e8a840dc1fb67305ebf05d58c8eb71
leta <980001stu.jnps@gmail.com>
2025-11-07 17:40:49 +0800

    Set new command line input

--- src/main.cpp
+++ src/main.cpp
@@ -22,38 +22,76 @@ int main(int argc, char* argv[]) {
 	}
 	else
 	{
-		// input parameters: circuit type, group , nfin, aspect, deviceNumList(at least 2)
-		if (argc < 7)
+		// old input parameters(mode = 0): mode, circuit_type, group , nfin, aspect, deviceNumList(at least 2) 
+		// new input parameters(mode = 1): mode, group, mos_weight, routing_track, horizontal_wire_limit, dummy_add_num, aspect_ratio, netList_file -> to do
+
+		if (argc < 2)
 		{
 			cout << "Error: input parameter too few" << endl;
+			return -1;
 		}
 
 
-		string circuitType = argv[1];
-		int group = stoi(argv[2]);
-		int nfin = stoi(argv[3]);
-		double aspect = stod(argv[4]);
-		vector<int> deviceNumList;
-		for (int i = 5; i < argc; i++)
+		string mode = argv[1];
+		if (mode == "0")
 		{
-			deviceNumList.push_back(stoi(argv[i]));
-		}
-		InItialPlacenent initialPlacement(circuitType, group, nfin, aspect, deviceNumList);
-		vector<TableManager> resultTables = initialPlacement.GetResultTable();
+			if (argc < 8)
+			{
+				cout << "Error: input parameter too few for mode 0" << endl;
+				return -1;
+			}
+			string circuitType = argv[2];
+			int group = stoi(argv[3]);
+			int nfin = stoi(argv[4]);
+			double aspect = stod(argv[5]);
+			vector<int> deviceNumList;
+			for (int i = 6; i < argc; i++)
+			{
+				deviceNumList.push_back(stoi(argv[i]));
+			}
+			InItialPlacenent initialPlacement(circuitType, group, nfin, aspect, deviceNumList);
+			vector<TableManager> resultTables = initialPlacement.GetResultTable();
 
-		Output result(circuitType, group, nfin, aspect, deviceNumList);
-		for (TableManager& r : resultTables)
+			Output result(circuitType, group, nfin, aspect, deviceNumList);
+			for (TableManager& r : resultTables)
+			{
+				// SA
+				cout << "Now Table Num: " << &r - &resultTables[0] + 1 << "/" << resultTables.size() << endl;
+				SAManager sa(r, 0.95, 100, 1, 100, true);
+				result.AddResultList(&r - &resultTables[0] + 1, sa.GetNondominatedSolution());
+				cout << "\n\tGet " << sa.GetNondominatedSolution().size() << " Nondominated Solutions.\n\n";
+			}
+
+			// output
+			result.PrintAllResult();
+			result.WriteAllResultToFile("output_file.txt");
+		}
+		else if (mode == "1")
 		{
-			// SA
-			cout << "Now Table Num: " << &r - &resultTables[0] + 1 << "/" << resultTables.size() << endl;
-			SAManager sa(r, 0.95, 100, 1, 100, true);
-			result.AddResultList(&r - &resultTables[0] + 1, sa.GetNondominatedSolution());
-			cout << "\n\tGet " << sa.GetNondominatedSolution().size() << " Nondominated Solutions.\n\n";
-		}
+			// input parameters(mode = 1) : mode, group, mos_weight, routing_track, horizontal_wire_limit, dummy_add_num, aspect_ratio, netList_file->to do
+			if (argc < 9)
+			{
+				cout << "Error: input parameter too few for mode 1" << endl;
+				return -1;
+			}
 
-		// output
-		result.PrintAllResult();
-		result.WriteAllResultToFile("output_file.txt");
+
+			int group = stoi(argv[2]);
+			double mosWeight = stod(argv[3]);
+			int routingTrack = stoi(argv[4]);
+			int horizontalWireLimit = stoi(argv[5]);
+			int dummyAddNum = stoi(argv[6]);
+			double aspectRatio = stod(argv[7]);
+			string netListFile = argv[8];
+
+
+
+		}
+		else
+		{
+			cout << "Error: mode input error" << endl;
+			return -1;
+		}
 	}
 
 	return 0;
